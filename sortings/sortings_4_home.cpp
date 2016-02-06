cc38316f59f39f77fed7b63fc83b42c485be96c2
duplyakin <duplyakinv@gmail.com>
2016-02-06 12:31:55 +0300

    fixed sortings_4_home.cpp

--- sortings/sortings_4_home.cpp
+++ sortings/sortings_4_home.cpp
@@ -9,40 +9,21 @@ and std::sort().
 #include <ctime>
 
 
-int GenerateArray() {
-  srand(clock());
-  return rand() % 10 + 1;
-}
-int GenerateSize() {
-  srand(clock());
+int generateElement() {
   return rand() % 100 + 1;
 }
-//int size = GenerateSize();
-
-
-void recovery(int array[], int arrayBeforeSorting[], int size) {
-	for (int i=0; i<size; i++) {
-		array[i] = arrayBeforeSorting[i];
-	}
-}
-
-
-void check(int array[], int arrayAfterRightSorting[], int size) {
-	for (int i=0; i<size; i++) {
-		if (array[i] != arrayAfterRightSorting[i]) {
-			WRN("Randomized sorting is incorrect!");
-		}
-	}
-	WRN("OK. Randomized sorting gives the same result as nonrandomised sorting");
+int generateSize() {
+  return rand() % 50000 + 1;
 }
 
 namespace sort {
+
+
 
 //nonrandomised pivot
 //------------------------------------------------------------------
 size_t partition(int array[], size_t p, size_t r) {
   int pivot = array[r - 1];
-  //int pivot = array[std::rand()%r];
   size_t i = p;
 
   for (size_t j = p + 1; j < r; ++j) {
@@ -57,24 +38,23 @@ size_t partition(int array[], size_t p, size_t r) {
 
 void quicksort(int array[], size_t p, size_t r) {
   if (p < r) {
-    size_t q = partition(array, p, r);//pivot //(quicksort, array, 4)(quicksort, array, 0, 4)(partition, array, 0, 4)... - NOT LAST
-    quicksort(array, p, q);//сортируется всё, что слева //(quicksort, array, 4)(quicksort, array, 0, 4)(quicksort, array, 0, 2)... - NOT LAST
-    quicksort(array, q + 1, r);//сортируется всё, что справа //(quicksort, array, 4)(quicksort, array, 0, 4)(quicksort, array, 3, 4)... - LAST - 
-    //хвостовая рекурсия. 
-   
+    size_t q = partition(array, p, r);
+    quicksort(array, p, q);
+    quicksort(array, q + 1, r);
   }
 }
 
-void quicksort(int array[], size_t size) { //(quicksort, array, 4)
-  quicksort(array, 0, size);//(quicksort, array, 4)(quicksort, array, 0, 4)
+void quicksort(int array[], size_t size) { 
+  quicksort(array, 0, size);
 }
 //---------------------------------------------------------------------
 
 //randomised pivot
 //------------------------------------------------------------------
-/*
-//https://en.wikipedia.org/wiki/Quicksort --- почему то не рабоает, возможно ответ здесь: http://www.cplusplus.com/forum/general/87796/
-int  Rpartition(int array[], int p, int r) {
+
+
+int partition1(int array[], int p, int r) {
+  
   int pivot = array[p];
   int i = p - 1;
   int j = r + 1;
@@ -91,90 +71,26 @@ int  Rpartition(int array[], int p, int r) {
   	else 
   		return j;
   }
-
 }
 
-void Rquicksort(int array[], int p, int r) {
+int partitionRand(int array[], int p, int r) {
+  int k = rand() % (r - p) + p; // Rand(p,r)
+  std::swap(array[k], array[p]);
+  return partition1(array, p, r);
+}
+
+void quicksortRand(int array[], int p, int r) {
   if (p < r) {
-    int q = Rpartition(array, p, r); 
-    Rquicksort(array, p, q); 
-    Rquicksort(array, q + 1, r);
+    int q = partitionRand(array, p, r); 
+    quicksortRand(array, p, q); 
+    quicksortRand(array, q + 1, r);
    
   }
 }
 
-void Rquicksort(int array[], int size) { 
-  Rquicksort(array, 2, size);
+void quicksortRand(int array[], int size) { 
+  quicksortRand(array, 0, size - 1);
 }
-*/
-
-/*
-//Почему то не работает... 
-int  Rpartition(int array[], int p, int r) {
-  int pivot = array[(p+r)/2];
-  int i = p;//если здесь сделать size_t i = p --- то будет ошибка
-  int j = r;
-  do 
-  {
-  	while (array[i] < pivot) i++;
-  	while (array[j] > pivot) j--;
-  	
-  	if (i <= j) {
-  		if (array[i] > array[j]) std::swap(array[i], array[j]);
-  		i++;
-  		j--;
-  	}	
-  } while (i <= j);
-
-}
-
-void Rquicksort(int array[], int p, int r) {
-   int i = p;
-   int j = r;
-   if (i < r)
-   	Rpartition(array, i, r);
-   if (p < j)
-   	Rpartition(array, p, j);
-}
-*/
-
-
-//http://forkettle.ru/vidioteka/programmirovanie-i-set/108-algoritmy-i-struktury-dannykh/sortirovka-i-poisk-dlya-chajnikov/1010-metod-khoara-bystraya-sortirovka-quick-sort
-void Rquicksort(int array[], size_t first, size_t last)
-{
-    size_t i = first, j = last; 
-    int x = array[(first + last) / 2];
-  
-    do {
-        while (array[i] < x) i++;
-        while (array[j] > x) j--;
-  
-        if(i <= j) {
-            if (array[i] > array[j]) std::swap(array[i], array[j]);
-            i++;
-            j--;
-        }
-    } while (i <= j);
-  
-    if (i < last)
-        Rquicksort(array, i, last);
-    if (first < j)
-        Rquicksort(array, first, j);
-}
-
-void Rquicksort(int array[], size_t size) { 
-  Rquicksort(array, 0, size-1);
-}
-
-/*
-Падает на следующих массивах: 
-49 51 -12 43 59 -12 -9 48 59 -18 4 0 0 10 57 16
-40 24 18 35 59 48 20 17 54 -14 50 19 5 8 17 30 -8 57 28 23 11 37 55 6 59 -8 49 -4 -3 6 24 23 59 -5 -2 29 54 37 -18
--15 -11 -16 -18 -13 
-
-*/
-
-//---------------------------------------------------------------------
 
 
 }
@@ -186,71 +102,104 @@ void print(int array[], size_t size) {
   printf("\n");
 }
 
+void unitTest() {
+  
+
+  clock_t start = clock();
+  clock_t elapsed = start;
+  while ((elapsed - start) < 5 * CLOCKS_PER_SEC) { 
+    int size = generateSize();
+    int array1[size];
+    int array2[size]; 
+    
+
+    for (int i = 0; i < size; i++) {
+      array1[i] = generateElement()%(2*40)-20;
+      array2[i] = array1[i];  
+    }
+
+    DBG("%i", size);
+
+    //INF("array_before_sorting:");
+    //print(array1, size);
+    sort::quicksort(array1, size);
+    //INF("array_after_random_sorting:");
+    //print(array1, size);
+
+    //INF("array_before_sorting:");
+    //print(array2, size);
+    sort::quicksortRand(array2, size);
+    //INF("array_after_random_sorting:");
+    //print(array2, size);
+
+    for (int i=0; i < size; ++i) {
+      if (array1[i] != array2[i]) {
+        ERR("Error: randomized and nonrandomised sortings give different results");
+        break;
+      }
+    }
+    INF("OK. randomized sorting gives the same result as nonrandomised sorting: %i", size);
+    elapsed = clock();
+  }
+}
+
+
+struct Score {
+  long int array1_wins;
+  long int array2_wins;
+};
+
+
+void performanceTest() {
+  struct Score score;
+  score.array1_wins = 0;
+  score.array2_wins = 0;
+  
+
+  clock_t start = clock();
+  clock_t elapsed = start;
+  while ((elapsed - start) < 5 * CLOCKS_PER_SEC) { 
+    int size = generateSize();
+    int array1[size];
+    int array2[size];
+
+    for (int i = 0; i < size; i++) {
+      array1[i] = generateElement()%(2*40)-20;
+      array2[i] = array1[i];  
+    }
+
+    DBG("%i", size);
+
+    clock_t sort1_start = clock();
+    sort::quicksort(array1, size);
+    clock_t sort1_elapsed = clock();
+
+    clock_t sort2_start = clock();
+    sort::quicksortRand(array2, size);
+    clock_t sort2_elapsed = clock();
+
+    double array2_time = static_cast<double>(sort2_elapsed - sort2_start) / CLOCKS_PER_SEC;
+    double array1_time = static_cast<double>(sort1_elapsed - sort1_start) / CLOCKS_PER_SEC; 
+    if (array1_time < array2_time) 
+      ++score.array1_wins;
+    else 
+      ++score.array2_wins;
+
+    elapsed = clock();
+  }
+  INF("Score: non-randomized quicksort is faster in [%ld] cases, randomized quicksort is faster in [%ld] cases", score.array1_wins, 
+    score.array2_wins);
+}
+
+
 int main(int argc, char** argv) {
   DBG("[Lesson 1]: Sortings: sortings_4_home");
-  /*for(int i=0; i<100; i++) {
-  	printf("%d ", std::rand()%10);
-  }
-  printf("\n");
-  */
-  int size = GenerateSize();
-  int array[size];
-  int arrayBeforeSorting[size];
-  int arrayAfterRightSorting[size];
-
-  for (int i=0; i<size; i++) {
-  	array[i] = GenerateArray()%(2*40)-20;
-  	arrayBeforeSorting[i] = array[i];
-  }
-
-  INF("Before sorting");
-  print(array, size);
-  INF("After nonrandomized sorting");
-  sort::quicksort(array, size);
-  print(array, size);
-  for (int i=0; i<size; i++) {//save result after right sorting
-  	arrayAfterRightSorting[i] = array[i];
-  }
-  //check(array, arrayAfterRightSorting, size);
-  recovery(array, arrayBeforeSorting, size);//восстанавливаем первоначальное значение массива
-
-  INF("Before sorting");
-  print(array, size);
-  INF("After randomized sorting");
-  sort::Rquicksort(array, size);
-  print(array, size);
-  check(array, arrayAfterRightSorting, size);
-  recovery(array, arrayBeforeSorting, size);
-  
  
-  //-------------------------------------------
-  recovery(array, arrayBeforeSorting, size);
-  INF("Time for nonrandomized sorting: ");
-  clock_t start = clock();
+  unitTest();
+  printf("Введите любой символ:");
+  getchar();
+  performanceTest();
  
-  sort::quicksort(array, size);
-  
-  clock_t end = clock();
-  int timeNonRandomozed = end-start/CLOCKS_PER_SEC;
-  printf("%d\n", timeNonRandomozed);
-  //-------------------------------------------
-  recovery(array, arrayBeforeSorting, size);
-  INF("Time for randomized sorting: ");
-  start = clock();
-  
-  sort::Rquicksort(array, size);
-  
-  end = clock();
-  int timeRandomozed = end-start/CLOCKS_PER_SEC;
-  printf("%d\n", timeRandomozed);
-  //-------------------------------------------
-  printf("More effective is: ");
-  if (timeRandomozed > timeNonRandomozed) {
-  	printf("nonrandomised sorting\n");
-  } else {
-  	printf("randomized sorting\n");
-  }
-
   DBG("[Lesson 1]: Sortings: sortings_4_home [END]");
   return 0;
 }
